3efff15550f8ffe2123df1e324ad0319e40986bd
sofia lagoa <lagoasofiaconstanza@gmail.com>
2022-08-27 12:37:56 -0300

    Complete index page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,24 @@
 import Button from "../../components/Button";
 import { useNavigate } from 'react-router-dom';
+import background from "../../assets/background.png";
 
-function Home (){
+function Home() {
     const navigate = useNavigate();
 
     const onClickButton = (url) => {
         navigate(url);
     }
 
-    return(
-        <>
-        <Button onClick={() => onClickButton('/game')} text='MetCamp Quiz' />
-        <Button onClick={() => onClickButton('/about')} text='About Me' />
-        </>
-     )
+    return (
+        <section className="hero is-medium is-fullheight" style={{backgroundImage: `url(${background})`, backgroundSize: `cover`}}>
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                <Button onClick={() => onClickButton('/game')} text='MetCamp Quiz' />
+                {/* <Button onClick={() => onClickButton('/about')} text='About Me' /> */}
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Home;
