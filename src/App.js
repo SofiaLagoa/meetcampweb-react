8558267ee33e5917b95f2696b26835c9fbe91388
sofia lagoa <lagoasofiaconstanza@gmail.com>
2022-09-02 19:03:43 -0300

    Update general

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Game from './pages/game';
-import About from './pages/about';
 import 'bulma/css/bulma.min.css';
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
         {/* game */}
         <Route path='/game' element={<Game />} />
         {/* about */}
-        <Route path='/about' element={<About />} />
+        {/* <Route path='/about' element={<About />} /> */}
       </Routes>
     </BrowserRouter>
   );
